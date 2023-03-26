8fb8c4f5cfbf061a06daeb01d2989a889a0825e0
upsiflu <upsiflu@gmail.com>
2023-03-26 23:53:33 +0200

    Add css effects

--- multimedia.ts
+++ multimedia.ts
@@ -1,5 +1,11 @@
 function run(): void {
   window["Elm"]?.Multimedia?.init();
+  const shellImages = document.querySelectorAll<HTMLElement>(":is(#Shell, #Restrictive) img");
+  window.addEventListener("mousemove", (e) => {
+    shellImages.forEach(shell => {
+      shell.style.filter = `hue-rotate(${~~(e.clientX / 2 + e.clientY / 3)}deg)`;
+    });
+  })
 }
 
 run();
